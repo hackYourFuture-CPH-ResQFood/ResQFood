165dbbe13a1c5e34aef8ef16990565a3c72df207
MKanunnikau <matea.ekv@gmail.com>
2026-06-29 11:07:49 +0200

    add styling to the component

--- components/storeCard/StoreCard.jsx
+++ components/storeCard/StoreCard.jsx
@@ -2,66 +2,60 @@ import styles from "./StoreCard.module.css";
 import StoreAvatar from "../ui/StoreAvatar/StoreAvatar";
 import Icon from "../ui/Icon/Icon";
 import { Route } from "lucide-react";
+import Badge from "../ui/Badge/Badge";
 
-const storeName = {
-  netto: "Netto",
-  foetex: "Føtex",
-  bilka: "Bilka",
-};
+export default function StoreCard({ store }) {
+  const {
+    name,
+    storeID,
+    address,
+    distance,
+    statusText,
+    statusCssClass,
+    offers,
+    hasBorder = false,
+  } = store;
 
-const getStoreName = (key) => {
-  return storeName[key.toLowerCase()] || key;
-};
-
-export default function StoreCard({ store, distance }) {
-  const displayName = getStoreName(store.brand);
   return (
     <>
-      <div className={styles.storeCard}>
+      <div className={`${styles.storeCard} ${hasBorder ? styles.border : ""}`}>
         <div className={styles.storeDetails}>
-          <StoreAvatar brand={"netto"} size={"md"} />
-          <p className={styles.storeName}>{displayName}</p>
-          <p className={styles.storeAddress}>{store.address.street}</p>
-          {distance && (
-            <div className={styles.distance}>
-              <Icon
-                icon={Route}
-                label="distance"
-                size="xs"
-                shape="square"
-                className={styles.distanceIcon}
-              />
-            </div>
-          )}
+          <StoreAvatar brand={storeID} size={"md"} />
         </div>
-        <div className={styles.workingDetails}></div>
-        <div className={styles.offerDetails}></div>
+        <div className={styles.storeInfo}>
+          <p className={styles.storeName}>{name}</p>
+          <div className={styles.location}>
+            <p className={styles.storeAddress}>{address}</p>
+
+            {distance && (
+              <div className={`${styles.distance} u-flex-center`}>
+                <Icon
+                  icon={Route}
+                  label="distance"
+                  size="xs"
+                  shape="none"
+                  className={styles.distanceIcon}
+                />
+                <p className={styles.distanceInfo}>{distance}</p>
+              </div>
+            )}
+          </div>
+          <div className={styles.workingDetails}>
+            <div className={`${styles.circle} ${styles[statusCssClass]}`}></div>
+            <p className={styles.openHours}>{statusText}</p>
+          </div>
+        </div>
+        <div className={styles.offerDetails}>
+          <Badge className="deals" variant="deals" size="lg">
+            <div className={styles.offerContainer}>
+              <p className={styles.offers}>{offers}</p>
+              <p className={styles.offersText}>
+                {offers === 1 ? "deal" : "deals"}
+              </p>
+            </div>
+          </Badge>
+        </div>
       </div>
     </>
   );
 }
-
-// store: {
-//       id: "5a5a3b85-1c8c-433e-a56c-23f6326306f7",
-//       address: {
-//         city: "Århus C",
-//         country: "DK",
-//         extra: null,
-//         street: "Finderupvej 1-3",
-//         zip: "8000",
-//       },
-//       brand: "netto",
-//       coordinates: [10.182309, 56.154791],
-//       hours: [
-//         {
-//           date: "2026-06-16",
-//           type: "store",
-//           open: "2026-06-16T07:00:00",
-//           close: "2026-06-16T23:00:00",
-//           closed: true,
-//           customerFlow: [
-//             0, 0, 0, 0, 0, 0, 0, 0.21, 0.21, 0.2, 0.26, 0.35, 0.38, 0.36, 0.44,
-//             0.53, 0.68, 0.78, 0.69, 0.49, 0.44, 0.51, 0.55, 0.02,
-//           ],
-//         },
-//     }
